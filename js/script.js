bb56b31297ef6e2ecbe412ac4c73cc90642a9a7a
Malgorzata Wilk <pocztamwilk@gmail.com>
2026-01-17 20:40:22 +0100

    Generate titlesList and active article

--- js/script.js
+++ js/script.js
@@ -45,46 +45,40 @@
   function generateTitleLinks (){
 
     /* remove contents of titleList */
+
     const titleList = document.querySelector(optTitleListSelector);
-    console.log('titleList is: ', titleList);
     titleList.innerHTML = '';
-    console.log('titleList is:', titleList);
 
     /* for each article */
+
     const articles = document.querySelectorAll(optArticleSelector);
-    console.log('articles are: ', articles);
 
     let html  = '';
 
     for(let article of articles){
 
       /* get the article id */
+
       const articleId = article.getAttribute('id');
-      console.log('articleId is: ', articleId);
 
       /* find the title element */
+
       const titleElement = article.querySelector(optTitleSelector);
-      console.log('titleElement is:', titleElement);
 
       /* get the title from the title element */
+
       const articleTitle = titleElement.innerHTML;
-      console.log('articleTitle is: ', articleTitle);
 
       /* create HTML of the link */
-      const linkHTML = '<li><a href="#' + articleId  + '"><span>' + articleTitle + '</span></a></li>';
-      console.log('linkHTML is: ', linkHTML);
 
-      /* insert link into titleList */
-      // titleList.insertAdjacentHTML("beforeend", linkHTML);
-      // console.log('TitleList is: ', titleList);
+      const linkHTML = '<li><a href="#' + articleId  + '"><span>' + articleTitle + '</span></a></li>';
 
       /* insert link into html variable */
+      
       html = html + linkHTML;
-      console.log('Value of html is: ', html);
     }
     titleList.innerHTML = html;
     const links = document.querySelectorAll('.titles a');
-    console.log('Const links is: ', links);
 
     for(let link of links){
       link.addEventListener('click', titleClickHandler);
